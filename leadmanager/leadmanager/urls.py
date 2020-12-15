00f91b17dcb527ee6cbf22fae5ab5773d01f917a
pawlaczyk <pawlaczyk@g.pl>
2020-12-15 01:01:14 +0100

    npm build dev

--- leadmanager/leadmanager/urls.py
+++ leadmanager/leadmanager/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include("frontend.urls")), # upewnic sie ze front sie laduje przed leads
     path('', include("leads.urls")),
 ]
