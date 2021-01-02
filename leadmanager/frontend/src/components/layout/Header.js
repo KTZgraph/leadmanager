8dfbc6780b7e8f6850002cc2fca2b7ff47158e70
pawlaczyk <pawlaczyk@g.pl>
2021-01-02 17:41:37 +0100

    React & Django [3] - Redux & HTTP

---
+++ leadmanager/frontend/src/components/layout/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+// rc tabulator - od razu robi szablon recta
+
+export default class Header extends Component {
+    render() {
+        return (
+               <nav className="navbar navbar-expand-sm navbar-light bg-light">
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a className="navbar-brand" >Lead Manager</a>
+                    <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+                    </ul>
+
+                
+                </div>
+                </nav> 
+        )
+    }
+}
